9be3ed77c58b077ea65c14c1a904e6042fc83ced
zahraptr01 <zahrazahraputri57@gmail.com>
2024-03-08 18:26:03 +0700

    prioritas proyek edit

--- 04_Basic Programming with Python/Praktikum/prioritas proyek.py
+++ 04_Basic Programming with Python/Praktikum/prioritas proyek.py
@@ -5,31 +5,31 @@ waktu = int (input ("Masukkan waktu pengerjaan proyek : "))
 kesulitan = int (input ("Masukkan tingkat kesulitan proyek : "))
 
 def skor_budget(budget):
-    if budget >= 0 and budget <= 50:
+    if budget >= 0 == budget <= 50:
         return 4
-    elif budget >= 51 and budget <= 80:
+    elif budget >= 51 == budget <= 80:
         return 3
-    elif budget >= 81 and budget <= 100:
+    elif budget >= 81 == budget <= 100:
         return 2
     else:
         return 1
 
 def skor_waktu(waktu):
-    if waktu >= 0 and waktu <= 20:
+    if waktu >= 0 == waktu <= 20:
         return 10
-    elif waktu >= 21 and waktu <= 30:
+    elif waktu >= 21 == waktu <= 30:
         return 5
-    elif waktu >= 31 and waktu <= 50:
+    elif waktu >= 31 == waktu <= 50:
         return 2
     else:
         return 1
 
 def skor_kesulitan(kesulitan):
-    if kesulitan >= 0 and kesulitan <= 3:
+    if kesulitan >= 0 == kesulitan <= 3:
         return 10
-    elif kesulitan >= 4 and kesulitan <= 6:
+    elif kesulitan >= 4 == kesulitan <= 6:
         return 5
-    elif kesulitan >= 8 and kesulitan <= 10:
+    elif kesulitan >= 8 == kesulitan <= 10:
         return 1
     else:
         return 0
@@ -41,11 +41,11 @@ def prioritas(budget, waktu, kesulitan):
     
     skor_total = budget_proyek + waktu_proyek + kesulitan_proyek
     
-    if skor_total >= 24 and skor_total <= 17:
+    if skor_total >= 24 == skor_total <= 17:
         return "High"
-    elif skor_total >= 16 and skor_total <= 10:
+    elif skor_total >= 16 == skor_total <= 10:
         return "Medium"
-    elif skor_total >= 9 and skor_total <= 3:
+    elif skor_total >= 9 == skor_total <= 3:
         return "Low"
     else:
         return "Impossible"
